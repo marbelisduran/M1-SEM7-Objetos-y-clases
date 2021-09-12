a9bb411d111e840cc165f8ed9b75476cb4982224
Marbelis <87458709+marbelisduran@users.noreply.github.com>
2021-09-11 20:31:09 -0500

    he actualizado index.html y main.js

---
+++ index.js
@@ -0,0 +1,27 @@
+/* fizzbuzz();
+
+console.og(mensaje): */
+
+function saludo () {
+    var mensaje = 'Hola que tal';
+    if (true) {
+        var mensje = 'Adios';
+        console.log(mensaje);
+    }
+    console.log(mensaje);
+}
+
+saludo
+
+console.log('########');
+
+function saludoLet () {
+    let mensaje = 'Hola que tal';
+    if (true) {
+        let mensje = 'Adios';
+        console.log(mensaje);
+    }
+    console.log(mensaje);
+}
+
+saludo
